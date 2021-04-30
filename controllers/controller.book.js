3682c702230097a3525135931a9f1ffb74c4a38f
nordluma <nordluma@arcada.fi>
2021-04-30 15:11:37 +0300

    get all books and add books

--- controllers/controller.book.js
+++ controllers/controller.book.js
@@ -5,7 +5,16 @@ const Book = require("../models/models.book");
 // @desc    Get all books
 // @route   GET /api/books
 // @access  Public
-const getAllBooks = async () => {};
+const getAllBooks = async () => {
+    try {
+        const books = await Book.find();
+        res.status(200).json(books);
+    } catch (err) {
+        const error = new Error(err);
+        error.status = err.status || 500;
+        next(error);
+    }
+};
 
 // @desc    Get book by id
 // @route   GET /api/books/:id
@@ -15,7 +24,23 @@ const getBookById = async () => {};
 // @desc    Add new book
 // @route   POST /api/books
 // @access  Public
-const addBook = async () => {};
+const addBook = async () => {
+    try {
+        const { title, author, description } = req.body;
+        const book = new Book({
+            _id: new mongoose.Types.ObjectId(),
+            author,
+            description,
+        });
+
+        const savedBook = await book.save();
+        return res.status(201).json({ message: "Book added", savedBook });
+    } catch (err) {
+        const error = new Error(err);
+        error.status = err.status || 500;
+        next(error);
+    }
+};
 
 // @desc    Update book info
 // @route   PATCH /api/books/:id
