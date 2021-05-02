1a63c60d855dff8118078802e557ae083e8a483b
nordluma <nordluma@arcada.fi>
2021-05-02 19:04:22 +0300

    added BookList, addBook

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { BookList } from "./components/BookList";
+import { AddBook } from "./components/AddBook";
+
 import { GlobalProvider } from "./context/GlobalState";
 
 import "./App.css";
 
 function App() {
-    return <GlobalProvider></GlobalProvider>;
+    return (
+        <GlobalProvider>
+            <div className="container">
+                <BookList />
+                <AddBook />
+            </div>
+        </GlobalProvider>
+    );
 }
 
 export default App;
