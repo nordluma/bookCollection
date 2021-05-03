61872fee927017db03699e618bb8f0d053ae8937
nordluma <nordluma@arcada.fi>
2021-05-03 18:50:18 +0300

    modal for addBook

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "./components/Header";
 import { BookList } from "./components/BookList";
 import { AddBook } from "./components/AddBook";
@@ -7,12 +7,22 @@ import { GlobalProvider } from "./context/GlobalState";
 import "./App.css";
 
 function App() {
+    const [showAddModal, setShowAddModal] = useState(false);
     return (
         <GlobalProvider>
             <Header />
             <div className="container">
+                <button
+                    onClick={() => setShowAddModal(true)}
+                    className="btn main-btn"
+                >
+                    Add New book
+                </button>
+                <AddBook
+                    open={showAddModal}
+                    onClose={() => setShowAddModal(false)}
+                />
                 <BookList />
-                <AddBook />
             </div>
         </GlobalProvider>
     );
