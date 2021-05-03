1ca263cb13ca4436dcf88fc56878be3d54163d1d
nordluma <nordluma@arcada.fi>
2021-05-03 18:52:46 +0300

    object destructuring and update book

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,28 +1,46 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
-    switch (action.type) {
+    const { type, payload } = action;
+
+    switch (type) {
         case "GET_BOOKS":
             return {
                 ...state,
                 loading: false,
-                books: action.payload,
+                books: payload,
+            };
+        case "UPDATE_BOOK":
+            return state.books.map((book) => {
+                if (book._id === payload._id) {
+                    return {
+                        ...book,
+                        ...payload,
+                    };
+                } else {
+                    return state;
+                }
+            });
+
+        case "GET_BOOK":
+            return {
+                ...state,
+                loading: false,
+                books: payload,
             };
         case "DELETE_BOOK":
             return {
                 ...state,
-                books: state.books.filter(
-                    (book) => book._id !== action.payload
-                ),
+                books: state.books.filter((book) => book._id !== payload),
             };
         case "ADD_BOOK":
             return {
                 ...state,
-                books: [...state.books, action.payload],
+                books: [...state.books, payload],
             };
         case "BOOK_ERROR":
             return {
                 ...state,
-                error: action.payload,
+                error: payload,
             };
         default:
             return state;
