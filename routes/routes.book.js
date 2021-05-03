9b9c21ab2e65152e8d3675a1126afa652aa1f00a
nordluma <nordluma@arcada.fi>
2021-05-03 13:56:35 +0300

    modified error handling

--- routes/routes.book.js
+++ routes/routes.book.js
@@ -14,11 +14,4 @@ router.post("/", addBook);
 router.patch("/:id", updateBook);
 router.delete("/:id", deleteBook);
 
-// Error handling
-router.use((req, res, next) => {
-    const error = new Error("Only GET, POST, PATCH, DELETE methods supported");
-    error.status = 500;
-    next(error);
-});
-
 module.exports = router;
--- server.js
+++ server.js
@@ -28,20 +28,6 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-// Error Handling
-app.use((req, res, next) => {
-    const error = new Error("Requested resource not found!");
-    error.status = 404;
-    next(error);
-});
-
-app.use((error, req, res) => {
-    res.status(error.status || 500).json({
-        status: error.status,
-        error: error.message,
-    });
-});
-
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
