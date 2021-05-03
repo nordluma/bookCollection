4441177bca17ab471bfab235a6f6091f5b642b35
nordluma <nordluma@arcada.fi>
2021-05-03 18:50:36 +0300

    changed into a modal

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
-export const AddBook = () => {
+export const AddBook = ({ open, onClose }) => {
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
     const [description, setDescription] = useState("");
@@ -22,41 +22,61 @@ export const AddBook = () => {
         setDescription("");
     };
 
+    if (!open) return null;
+
     return (
         <>
-            <h3>Add new book</h3>
-            <form onSubmit={onSubmit}>
-                <div className="form-control">
-                    <label htmlFor="title">Title</label>
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Enter title"
-                    />
+            <div className="overlay">
+                <div className="modal">
+                    <h3>Add new book</h3>
+                    <form onSubmit={onSubmit}>
+                        <div className="form-control">
+                            <label htmlFor="title">Title</label>
+                            <input
+                                type="text"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                placeholder="Enter title"
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="author">Author</label>
+                            <input
+                                type="text"
+                                value={author}
+                                onChange={(e) => setAuthor(e.target.value)}
+                                placeholder="Enter author"
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="description">Description</label>
+                            <textarea
+                                id="description"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                placeholder="Enter description"
+                                cols="30"
+                                rows="10"
+                            ></textarea>
+                        </div>
+                        <div className="btn-container">
+                            <div className="btn-inner-container">
+                                <button className="btn add-btn">
+                                    Add Book
+                                </button>
+                            </div>
+                            <div className="btn-inner-container">
+                                <button
+                                    onClick={onClose}
+                                    className="btn close-btn"
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div className="form-control">
-                    <label htmlFor="author">Author</label>
-                    <input
-                        type="text"
-                        value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
-                        placeholder="Enter author"
-                    />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="description">Description</label>
-                    <textarea
-                        id="description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Enter description"
-                        cols="30"
-                        rows="10"
-                    ></textarea>
-                </div>
-                <button className="btn">Add Book</button>
-            </form>
+            </div>
         </>
     );
 };
