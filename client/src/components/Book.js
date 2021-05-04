31201a5c04e96c48432f8d72a37298222fcf298b
nordluma <nordluma@arcada.fi>
2021-05-04 15:39:27 +0300

    book component includes a modal for information

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -4,28 +4,62 @@ import { UpdateModal } from "../components/UpdateModal";
 
 export const Book = ({ book }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [showDescription, setShowDescription] = useState(false);
     const { deleteBook } = useContext(GlobalContext);
 
-    return (
-        <li className={book.title}>
-            <h3>{book.title}</h3>
-            <span>{book.author}</span>
-            <p>{book.description}</p>
-            <button onClick={() => setIsOpen(true)} className="btn update-btn">
-                Update
-            </button>
-            <UpdateModal
-                book={book}
-                open={isOpen}
-                onClose={() => setIsOpen(false)}
-            />
+    if (!showDescription) {
+        return (
+            <li className={book.title}>
+                <h3>{book.title}</h3>
+                <span>{book.author}</span>
+                <button
+                    onClick={() => setShowDescription(true)}
+                    className="btn show-btn"
+                >
+                    Show more
+                </button>
+            </li>
+        );
+    }
 
-            <button
-                onClick={() => deleteBook(book._id)}
-                className="btn delete-btn"
-            >
-                Delete
-            </button>
-        </li>
+    return (
+        <>
+            <li className={book.title}>
+                <h3>{book.title}</h3>
+                <span>{book.author}</span>
+                <p>{book.description}</p>
+                <UpdateModal
+                    book={book}
+                    open={isOpen}
+                    onClose={() => setIsOpen(false)}
+                />
+            </li>
+            <div className="btn-container">
+                <div className="btn-inner-container">
+                    <button
+                        onClick={() => setIsOpen(true)}
+                        className="btn update-btn"
+                    >
+                        Update
+                    </button>
+                </div>
+                <div className="btn-inner-container">
+                    <button
+                        onClick={() => deleteBook(book._id)}
+                        className="btn delete-btn"
+                    >
+                        Delete
+                    </button>
+                </div>
+                <div className="btn-inner-container">
+                    <button
+                        onClick={() => setShowDescription(false)}
+                        className="btn close-btn"
+                    >
+                        Close
+                    </button>
+                </div>
+            </div>
+        </>
     );
 };
